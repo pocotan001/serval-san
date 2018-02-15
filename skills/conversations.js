a86ac98c7fc2b83e739a96bb33c65def694b18ee
pocotan001 <pocotan001@gmail.com>
2018-02-15 16:38:02 +0900

    🚧

--- skills/conversations.js
+++ skills/conversations.js
@@ -8,8 +8,8 @@ module.exports = controller => {
   controller.hears(
     "(.+)",
     ["direct_message", "direct_mention", "mention"],
-    async (bot, message) => {
-      bot.api.users.info({ user: message.user }, async (err, res) => {
+    (bot, message) => {
+      bot.api.users.info({ user: message.user }, (err, res) => {
         if (err) {
           bot.botkit.log("Failed to fetch user information", err);
           return;
@@ -17,28 +17,26 @@ module.exports = controller => {
 
         bot.reply(message, JSON.stringify(message, null, 2));
 
-        try {
-          const response = await fetch(dialogueApiUrl, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              // システムから出力された context を入力することにより会話を継続します
-              context,
-              // ユーザの発話を入力します
-              utt: message.text,
-              // ユーザのニックネームを設定します
-              nickname: message.user
-            })
+        fetch(dialogueApiUrl, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            // システムから出力された context を入力することにより会話を継続します
+            context,
+            // ユーザの発話を入力します
+            utt: message.text,
+            // ユーザのニックネームを設定します
+            nickname: message.user
+          })
+        })
+          .then(res => res.json())
+          .catch(err => bot.botkit.log(err))
+          .then(res => {
+            context = json.context;
+            bot.reply(message, json.utt);
+
+            bot.reply(json, JSON.stringify(message, null, 2));
           });
-          const json = await response.json();
-
-          bot.reply(json, JSON.stringify(message, null, 2));
-
-          context = json.context;
-          bot.reply(message, json.utt);
-        } catch (e) {
-          bot.botkit.log(e);
-        }
       });
     }
   );
