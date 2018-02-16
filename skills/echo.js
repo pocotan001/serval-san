2dafd59c457d3ad20e5c9f771e6f47715dfac2f2
pocotan001 <pocotan001@gmail.com>
2018-02-16 15:53:39 +0900

    updates

--- skills/echo.js
+++ skills/echo.js
@@ -1,10 +1,7 @@
 module.exports = controller => {
-  controller.on("slash_command", (bot, message) => {
-    // reply to slash command
-    bot.replyPublic(message, "Everyone can see this part of the slash command");
-    bot.replyPrivate(
-      message,
-      "Only the person who used the slash command can see this."
-    );
+  controller.hears("^echo #(.+?) (.+)", "direct_message", (bot, message) => {
+    const [, channel, text] = message.match;
+
+    bot.say({ text, channel });
   });
 };
