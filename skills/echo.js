8c378420b4637d72b0e73bab74ef540446df030f
pocotan001 <pocotan001@gmail.com>
2018-02-16 17:15:34 +0900

    updates

--- skills/echo.js
+++ skills/echo.js
@@ -1,5 +1,5 @@
 module.exports = controller => {
-  controller.hears("^echo #(.+?) (.+)", "direct_message", (bot, message) => {
+  controller.hears("^echo (.+?) (.+)", "direct_message", (bot, message) => {
     const [, channel, text] = message.match;
 
     bot.say({ text, channel });
